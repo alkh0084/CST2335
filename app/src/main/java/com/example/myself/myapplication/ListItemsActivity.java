ad86039ae92d1786459ff2c10a8d216f1ef015ac
alkh0084 <alkh0084@algonquinlive.com>
2018-02-02 00:50:26 -0500

    Intent and sharepreferences

--- app/src/main/java/com/example/myself/myapplication/ListItemsActivity.java
+++ app/src/main/java/com/example/myself/myapplication/ListItemsActivity.java
@@ -2,12 +2,36 @@ package com.example.myself.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ListItemsActivity extends Activity {
+    protected static final String ListItemsActivity = "ListItemsActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_items);
+    protected void onResume(){
+        super.onResume();
+
+        Log.i("ListItemsActivity", "In OnResume()");
     }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("ListItemsActivity", "In onStart()");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("ListItemsActivity", "In onPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("ListItemsActivity", "In onStop()");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("ListItemsActivity", "In onDestroy()");
+    }
+
 }
--- app/src/main/java/com/example/myself/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myself/myapplication/LoginActivity.java
@@ -1,13 +1,56 @@
 package com.example.myself.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
-
+    protected static final String LoginActivity = "LoginActivity";
+    EditText editText;
+    Button button;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    protected void onResume(){
+        super.onResume();
+        Log.i("LoginActivity", "In OnResume()");
+        editText=(EditText) findViewById(R.id.emailLogin);
+        button = (Button)findViewById(R.id.button);
+        saveInfo();
+        Intent intent=new Intent(LoginActivity.this, StartActivity.class);
+        startActivity(intent);
+
     }
+    public void saveInfo(){
+        SharedPreferences sharedPreferences = getSharedPreferences("usersEmail", Context.MODE_PRIVATE);
+        sharedPreferences.getString("DefaultEmail","email@domain.com");
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("Username",editText.getText().toString());
+        editor.apply();
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("LoginActivity", "In onStart()");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("LoginActivity", "In onPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("LoginActivity", "In onStop()");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("LoginActivity", "In onDestroy()");
+    }
+
+
 }
